01a2c9464ba96b654f5d9fb62f5a673eedfbc7f2
SantAnna <juper.sant@gmail.com>
2021-11-26 17:55:21 -0300

    exercicios de python do curso em video

---
+++ desafio001.py
@@ -0,0 +1,5 @@
+# == DESAFIO 001 ==
+# Crie um script Python que leia o nome de uma pessoa e mostre uma mensagem de boas-vindas de acordo com o valor digitado.
+
+nome = input('Bem-vindo(a)!Qual é o seu nome?')
+print('Bem-vindo(a)',nome,'!')
---
+++ desafio002.py
@@ -0,0 +1,8 @@
+# == DESAFIO 002 ==
+# Crie um script Python que leia o dia, o mês e o ano de nascimento de uma pessoa e mostre uma mensagem com a data formatada.
+
+print('Olá. Qual é a sua data de nascimento?')
+dia = input('Dia = ')
+mes = input('Mês = ')
+ano = input('Ano = ')
+print('Você nasceu no dia', dia, 'de', mes, 'de', ano, ', Correto?')
