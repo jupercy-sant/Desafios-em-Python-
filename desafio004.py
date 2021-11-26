494c3c2d8b77f6f67e89d9998091ec19364e13ab
SantAnna <86057706+jupercy-sant@users.noreply.github.com>
2021-11-26 19:28:18 -0300

    Add files via upload

---
+++ desafio004.py
@@ -0,0 +1,12 @@
+# == Desafio 004 ==
+# Faça um programa que leia algo pelo teclado e mostre na tela o seu tipo primitivo e todas as informações possíveis sobre ele.
+
+n1 = input('Digite algo: ')
+print('O tipo primitivo do que foi digitado é ', type(n1))
+print('É um número? ', n1.isnumeric())
+print('É alfabético? ', n1.isalpha())
+print('É alfanumérico? ', n1.isalnum())
+print('Só tem espaços?', n1.isspace())
+print('Está em maiúsculo? ', n1.isupper())
+print('Está em minúsculas? ', n1.islower())
+print('Está captalizada? ', n1.istitle())
